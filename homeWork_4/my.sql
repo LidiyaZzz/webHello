26efb01a8f3940de1a382bd9435b9e413f5d93d6
Lida <lida>
2023-06-05 00:48:41 +0700

    fix

--- homeWork_4/my.sql
+++ homeWork_4/my.sql
@@ -1,21 +1,21 @@
-CREATE TABLE employee (
+CREATE TABLE students (
     id integer PRIMARY KEY,
     name text NOT NULL,
     age integer NOT NULL,
     address text NOT NULL
 );
 
-INSERT INTO employee VALUES (1, "Vladislav", 23, "Saratov");
-INSERT INTO employee VALUES (2, "Alina", 28, "Moscow");
-INSERT INTO employee VALUES (3, "Igor", 32, "Kemerovo");
-INSERT INTO employee VALUES (4, "Tatyana", 18, "Irkutsk");
-INSERT INTO employee VALUES (5, "Dmitry", 27, "Voronezh");
-INSERT INTO employee VALUES (6, "Alexandr", 37, "Saint Petersburg");
-INSERT INTO employee VALUES (7, "Irina", 26, "Moscow");
-INSERT INTO employee VALUES (8, "Olga", 26, "Kemerovo");
-INSERT INTO employee VALUES (9, "Timofey", 36, "Moscow");
+INSERT INTO students VALUES (1, "Vladislav", 23, "Saratov");
+INSERT INTO students VALUES (2, "Alina", 28, "Moscow");
+INSERT INTO students VALUES (3, "Igor", 32, "Kemerovo");
+INSERT INTO students VALUES (4, "Tatyana", 18, "Irkutsk");
+INSERT INTO students VALUES (5, "Dmitry", 27, "Voronezh");
+INSERT INTO students VALUES (6, "Alexandr", 37, "Saint Petersburg");
+INSERT INTO students VALUES (7, "Irina", 26, "Moscow");
+INSERT INTO students VALUES (8, "Olga", 26, "Kemerovo");
+INSERT INTO students VALUES (9, "Timofey", 36, "Moscow");
 
-SELECT name FROM employee WHERE age >= 18 AND age < 30 AND address = "Moscow";
+SELECT name FROM students WHERE age >= 18 AND age < 30 AND address = "Moscow";
 
 
 -- id, name, age, address 
